54c0bcbde433eb6221d2083d1bba673f7f77e5ec
Lisa <Lisa>
2016-11-07 05:13:23 +0200

    securing site. step first corrected

--- src/main/java/fi/haagahelia/course/WebSecurityConfig.java
+++ src/main/java/fi/haagahelia/course/WebSecurityConfig.java
@@ -15,7 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            	.antMatchers("/", "add", "save", "delete/{id}").permitAll()
+            	
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
